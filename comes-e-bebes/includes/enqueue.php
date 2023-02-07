f1bb73d6c876df19a47a7bc7b83718307e96b32c
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-02-07 15:17:20 -0300

    feat: Atualização

--- comes-e-bebes/includes/enqueue.php
+++ comes-e-bebes/includes/enqueue.php
@@ -1,16 +1,12 @@
 <?php
 
-function NOME???_enqueue_style(){
+function comesbebes_enqueue_style(){
     wp_register_style('comesbebes-reset', STYLE_DIR . '/reset.css', [], '1.0.0', false);
-    wp_register_style(
-        LINK DE FONTE SE NECESSARIO
-    );
     wp_register_style('style', get_stylesheet_uri(), [], '1.0.0', false);
     wp_register_style('comesbebes-header', STYLE_DIR . '/header.css', [], '1.0.0', false);
 
 
     wp_enqueue_style('comesbebes-reset');
-    wp_enqueue_style('NOME DE FONTE SE NECESSARIO');
     wp_enqueue_style('style');
     wp_enqueue_style('comesbebes-header');
 }
