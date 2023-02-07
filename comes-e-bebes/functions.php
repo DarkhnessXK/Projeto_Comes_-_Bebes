2fbbac5235a6834b1d9ccec7d37e6fc37f6120a1
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-02-06 23:05:23 -0300

    feat: Tema comes-e-bebe

---
+++ comes-e-bebes/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+//Variáveis
+
+define('ROOT_DIR', get_theme_file_path());
+define('STYLE_DIR', get_template_directory_uri() . '/assets/css');
+define('IMAGES_DIR', get_template_directory_uri() . '/assets/images');
+define('INCLUDES_DIR', ROOT_DIR . '/includes');
+
+
+//Includes
+
+include_once(INCLUDES_DIR . '/enqueue.php');
+include_once(INCLUDES_DIR . '/setup-theme.php');
+
+
+//Ganchos
+
+add_action('wp_enqueue_scripts', 'comesbebes_enqueue_style');
+add_action('after_theme_support', 'comesbebes_setup-theme');
+
+?>
