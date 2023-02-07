d58cd223aa8e9276b483fec015db133adef7c50f
Matheus Ferreira <theliomatheus@protonmail.com>
2023-02-07 15:34:53 -0300

    feat: google fonts preconnect

--- comes-e-bebes/header.php
+++ comes-e-bebes/header.php
@@ -3,10 +3,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
     <title><?php bloginfo('name'); ?> | <?php the_title(); ?></title>
     <?php wp_head(); ?>
     
 </head>
 <body <?php body_class(); ?>>
 
-    <header>
+
+<header>
+    HEADER
+</header>
+
