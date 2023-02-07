9ad31bb4e81389b3fe85ec54aff558bf9ace54b1
DarkhnessXK <giovannitoledo@id.uff.br>
2023-02-07 17:37:33 -0300

    fix: arquive products aceitando codigo

--- comes-e-bebes/functions.php
+++ comes-e-bebes/functions.php
@@ -8,6 +8,12 @@ define('IMAGES_DIR', get_template_directory_uri() . '/assets/images');
 define('INCLUDES_DIR', ROOT_DIR . '/includes');
 
 
+
+function comes_e_bebes_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+
 //Includes
 
 include_once(INCLUDES_DIR . '/enqueue.php');
@@ -18,11 +24,10 @@ include_once(INCLUDES_DIR . '/setup-theme.php');
 
 add_action('wp_enqueue_scripts', 'comesbebes_enqueue_style');
 add_action('after_theme_support', 'comesbebes_setup-theme');
-
-
-function comes_e_bebes_add_woocommerce_support() {
-    add_theme_support( 'woocommerce' );
-}
 add_action( 'after_setup_theme', 'comes_e_bebes_add_woocommerce_support' );
+
+
+
+
 
 ?>
