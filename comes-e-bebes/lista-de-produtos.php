44b99c19e774e4fb16c1243921ce064c3e74b73a
DarkhnessXK <giovannitoledo@id.uff.br>
2023-02-07 16:36:33 -0300

    feat: sections de categoria e comida

---
+++ comes-e-bebes/lista-de-produtos.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php bloginfo('name'); ?> |
+        <?php the_title(); ?>
+    </title>
+</head>
+
+<body>
+
+    <header>
+
+    </header>
+
+    <main>
+        <section class="category-section">
+            <h2>Escolha uma Categoria</h2>
+            <div class="category-cards">
+                <img>
+                <img>
+                <img>
+                <img>
+            </div>
+        </section>
+
+        <section class="foods-section">
+            <h2>Pratos</h2>
+            <h3>Comida (da categoria)</h3>
+
+            <p>Buscar por nome:</p>
+            <input>
+        </section>
+
+    </main>
+
+
+    <footer>
+    </footer>
+</body>
+
+</html>
