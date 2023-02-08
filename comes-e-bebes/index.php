8384bf088f681e2a4108041ffd3f0e610606fe73
Júlia Câmara <juliasgomes2002@gmail.com>
2023-02-07 22:19:44 -0300

    feat(home-page): add bigger footer

--- comes-e-bebes/index.php
+++ comes-e-bebes/index.php
@@ -8,6 +8,3 @@ INDEX
 
 
 <?php get_footer();?>
-
-
-
--- comes-e-bebes/page-home.php
+++ comes-e-bebes/page-home.php
@@ -3,11 +3,14 @@
 
 
 
-HOME
-
-
-
+<div class="footer-home">
+    <h3>VISITE NOSSA LOJA FÍSICA</h3>
+    <div>
+        <div class="map">
+            <div style="overflow:hidden;width:250px;position:relative;"></div>
+            <iframe width="345px" height="203px" src="https://maps.google.com/maps?width=250&amp;height=168&amp;hl=en&amp;q=&amp;t=&amp;z=12&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        </div>
+        <img id="footer_img" src="<?php echo IMAGES_DIR . '/happyfriends.jpg' ?>" alt="happy friends">
+    </div>
+</div>
 <?php get_footer();?>
-
-
-
