49335b439a739896d7c3bf5fc4caca156f82ccee
Matheus Ferreira <theliomatheus@protonmail.com>
2023-02-08 16:37:18 -0300

    feat: added cart

--- comes-e-bebes/functions.php
+++ comes-e-bebes/functions.php
@@ -5,6 +5,7 @@
 define('ROOT_DIR', get_theme_file_path());
 define('STYLE_DIR', get_template_directory_uri() . '/assets/css');
 define('IMAGES_DIR', get_template_directory_uri() . '/assets/images');
+define('JS_DIR', get_template_directory_uri() . '/assets/js');
 define('INCLUDES_DIR', ROOT_DIR . '/includes');
 
 
@@ -18,5 +19,8 @@ include_once(INCLUDES_DIR . '/setup-theme.php');
 
 add_action('wp_enqueue_scripts', 'comesbebes_enqueue_style');
 add_action('after_setup_theme', 'comesbebes_setup_theme');
+
+add_action( 'wp_enqueue_scripts', 'comesbebes_enqueue_scripts' );
+
 
 ?>
--- comes-e-bebes/includes/enqueue.php
+++ comes-e-bebes/includes/enqueue.php
@@ -1,14 +1,16 @@
 <?php
 
 function comesbebes_enqueue_style(){
+    
     wp_register_style('comesbebes-reset', STYLE_DIR . '/reset.css', [], '1.0.0', false);
     
-    wp_register_style('comesbebes-bellota-text-font', 'https://fonts.googleapis.com/css2?family=Bellota+Text:wght@300&display=swap');
+    wp_register_style('comesbebes-bellota-text-font', 'https://fonts.googleapis.com/css2?family=Bellota+Text:wght@300;400;700&display=swap');
     wp_register_style('comesbebes-roboto-font', 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;900&display=swap');
 
     
     wp_register_style('style', get_stylesheet_uri(), [], '1.0.0', false);
     wp_register_style('comesbebes-header', STYLE_DIR . '/header.css', [], '1.0.0', false);
+    wp_register_style('comesbebes-cart', STYLE_DIR . '/cart.css', [], '1.0.0', false);
     wp_register_style('comesbebes-home', STYLE_DIR . '/home.css', [], '1.0.0', false);
     wp_register_style('comesbebes-painel', STYLE_DIR . '/painel.css', [], '1.0.0', false);
 
@@ -20,10 +22,15 @@ function comesbebes_enqueue_style(){
 
     wp_enqueue_style('style');
     wp_enqueue_style('comesbebes-header');
+    wp_enqueue_style('comesbebes-cart');
     wp_enqueue_style('comesbebes-home');
     wp_enqueue_style('comesbebes-painel');
 }
 
+function comesbebes_enqueue_scripts(){
+    wp_enqueue_script( 'comesbebes_cart', JS_DIR . '/cart.js', array(), '1.0.0', true );
+
+}
 
 
 ?>
