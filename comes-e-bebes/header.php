22c56ad83ac2ac0f748a920b2d9e649a4e551418
Júlia Câmara <juliasgomes2002@gmail.com>
2023-02-07 23:36:19 -0300

    refactor(all): update

--- comes-e-bebes/header.php
+++ comes-e-bebes/header.php
@@ -1,20 +1,32 @@
+<!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
     <title><?php bloginfo('name'); ?> | <?php the_title(); ?></title>
     <?php wp_head(); ?>
-    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 </head>
 <body <?php body_class(); ?>>
 
 
 <header>
-    HEADER
+    <div class="inner_header">
+        <div class="left_header">
+            <?php the_custom_logo();?>
+            <?php get_search_form(); ?>
+        </div>
+
+
+        <div class="rigth_header">
+            
+            <div class="pedido_button"><a href="/">Faça um pedido</a></div>
+            <a href="/"><img class="header_icon" src="<?php echo IMAGES_DIR . "/cart.png"?>" alt=""></a>
+            <a href="/"><img class="header_icon" src="<?php echo IMAGES_DIR . "/account.png"?>" alt=""></a>
+
+        </div>
+    </div>
 </header>
 
--- comes-e-bebes/includes/enqueue.php
+++ comes-e-bebes/includes/enqueue.php
@@ -2,21 +2,25 @@
 
 function comesbebes_enqueue_style(){
     wp_register_style('comesbebes-reset', STYLE_DIR . '/reset.css', [], '1.0.0', false);
+    
+    wp_register_style('comesbebes-bellota-text-font', 'https://fonts.googleapis.com/css2?family=Bellota+Text:wght@400&display=swap');
+    wp_register_style('comesbebes-roboto-font', 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;900&display=swap');
+
     wp_register_style('style', get_stylesheet_uri(), [], '1.0.0', false);
     wp_register_style('comesbebes-header', STYLE_DIR . '/header.css', [], '1.0.0', false);
     wp_register_style('comesbebes-footer', STYLE_DIR . '/footer.css', [], '1.0.0', false);
-    wp_register_style(
-        'belota-text',
-        'https://fonts.googleapis.com/css2?family=Bellota+Text:wght@400;700&family=Darker+Grotesque:wght@300;400;700&display=swap'
-    );
-
+    wp_register_style('comesbebes-home', STYLE_DIR . '/home.css', [], '1.0.0', false);
+    wp_register_style('comesbebes-painel', STYLE_DIR . '/painel.css', [], '1.0.0', false);
+    
+    wp_enqueue_style('comesbebes-bellota-text-font');
+    wp_enqueue_style('comesbebes-roboto-font');
+    
     wp_enqueue_style('comesbebes-reset');
     wp_enqueue_style('style');
     wp_enqueue_style('comesbebes-header');
     wp_enqueue_style('comesbebes-footer');
-    wp_enqueue_style('belota-text');
+    wp_enqueue_style('comesbebes-home');
+    wp_enqueue_style('comesbebes-painel');
 }
-
-
 
 ?>
