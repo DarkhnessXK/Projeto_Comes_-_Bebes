3ad3f098f5ac4168aeefb4f38013f71c60fc18de
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-02-07 22:56:11 -0300

    feat: att setup-theme

--- comes-e-bebes/includes/setup-theme.php
+++ comes-e-bebes/includes/setup-theme.php
@@ -6,7 +6,8 @@ function comesbebes_setup_theme() {
     //add_theme_support('menus');
 
     register_nav_menus([
-        "Direita" => "Menu Lateral Direito"
+        "Direita" => "Menu Lateral Direito",
+        "Esquerda" => "Menu Lateral Esquerdo"
     ]);
 
 }
