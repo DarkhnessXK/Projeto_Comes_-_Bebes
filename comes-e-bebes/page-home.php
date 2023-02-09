7e6e8080abf63dcc0d5058f11786055ea9d57fdb
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-02-08 23:33:23 -0300

    feat: Atualização página page-home

--- comes-e-bebes/page-home.php
+++ comes-e-bebes/page-home.php
@@ -10,21 +10,38 @@
 
     <div class="pratos-principais">
         <h4>Tipo de pratos principais</h4>
-        <a href="/"><img class="tipos" src="<?php echo IMAGES_DIR . "/nordestina.jpg"?>" alt="Comida Nordestina"></a>
-        <a href="/"><img class="tipos" src="<?php echo IMAGES_DIR . "/vegana.jpg"?>" alt="Comida Vegana"></a>
-        <a href="/"><img class="tipos" src="<?php echo IMAGES_DIR . "/massas.jpg"?>" alt="Massas"></a>
-        <a href="/"><img class="tipos" src="<?php echo IMAGES_DIR . "/japonesa.jpg"?>" alt="Comida Japonesa"></a>
+
+        <a href="/"><img class="pratos-nomes" src="<?php echo IMAGES_DIR . "/gradienteroxo.png"?>"><img class="tipos" src="<?php echo IMAGES_DIR . "/nordestina.jpg"?>" alt="Comida Nordestina">
+        </a>            
+
+        <a href="/"><img class="pratos-nomes" src="<?php echo IMAGES_DIR . "/gradienteroxo.png"?>"><img class="tipos" src="<?php echo IMAGES_DIR . "/vegana.jpg"?>" alt="Comida Vegana">
+        </a>
+
+        <a href="/"><img class="pratos-nomes" src="<?php echo IMAGES_DIR . "/gradienteroxo.png"?>"><img class="tipos" src="<?php echo IMAGES_DIR . "/massas.jpg"?>" alt="Massas">
+        </a>
+
+        <a href="/"><img class="pratos-nomes" src="<?php echo IMAGES_DIR . "/gradienteroxo.png"?>"><img class="tipos" src="<?php echo IMAGES_DIR . "/japonesa.jpg"?>" alt="Comida Japonesa">
+        </a>
     </div>
 
     <div class="pratos-do-dia">
         <h4>Pratos dos dias de hoje: </br>
             <?php setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
             date_default_timezone_set('America/Sao_Paulo');
-            echo ucwords(strftime('%A', strtotime('today')));?></h4>
-        <a href="/"><img class="tipos2" src="<?php echo IMAGES_DIR . "/pratododia1.jpg"?>" alt="Prato do dia  1"></a>
-        <a href="/"><img class="tipos2" src="<?php echo IMAGES_DIR . "/pratododia2.jpg"?>" alt="Prato do dia 2"></a>
-        <a href="/"><img class="tipos2" src="<?php echo IMAGES_DIR . "/pratododia3.jpg"?>" alt="Prato do dia 3"></a>
-        <a href="/"><img class="tipos2" src="<?php echo IMAGES_DIR . "/pratododia4.jpg"?>" alt="Prato do dia 4"></a>
+            echo ucwords(strftime('%A', strtotime('today')));?>
+        </h4>
+
+        <a href="/"><img class="pratos-dia-nomes" src="<?php echo IMAGES_DIR . "/gradienteroxo_grande.png"?>"><img class="tipos2" src="<?php echo IMAGES_DIR . "/pratododia1.jpg"?>" alt="Prato do dia  1">
+        </a>
+
+        <a href="/"><img class="pratos-dia-nomes" src="<?php echo IMAGES_DIR . "/gradienteroxo_grande.png"?>"><img class="tipos2" src="<?php echo IMAGES_DIR . "/pratododia2.jpg"?>" alt="Prato do dia 2">
+        </a>
+
+        <a href="/"><img class="pratos-dia-nomes" src="<?php echo IMAGES_DIR . "/gradienteroxo_grande.png"?>"><img class="tipos2" src="<?php echo IMAGES_DIR . "/pratododia3.jpg"?>" alt="Prato do dia 3">
+        </a>
+
+        <a href="/"><img class="pratos-dia-nomes" src="<?php echo IMAGES_DIR . "/gradienteroxo_grande.png"?>"><img class="tipos2" src="<?php echo IMAGES_DIR . "/pratododia4.jpg"?>" alt="Prato do dia 4">
+        </a>
     </div>
 
     <div class="botao-opcoes">
